a731f177fb44afac3a986b4415b7192ffdd12eb3
Sam <soc27@humboldt.edu>
2017-08-14 12:21:31 -0700

    Speed up gif creation process

--- gif.py
+++ gif.py
@@ -25,112 +25,87 @@ class BitString:
 
 class Gif:
     def __init__(self, width, height, colors):
-        self.width = width
-        self.height = height
-        self.colors = colors
+        self.width, self.height, self.colors = width, height, colors
         self.color_map = {color: i for i, color in enumerate(colors)}
-        self.pixels = [[0 for y in range(height)] for x in range(width)]
-        self.frames = []
+        self.cur_frame = [[0 for y in range(height)] for x in range(width)]
+        self.content = bytes()
+        self.write_header()
+        self.diff_x1 = 0
+        self.diff_y1 = 0
+        self.diff_x2 = self.width - 1
+        self.diff_y2 = self.height - 1
+
+    def add_diff(self, x, y):
+        if self.diff_x1 is None:
+            self.diff_x1 = self.diff_x2 = x
+            self.diff_y1 = self.diff_y2 = y
+        else:
+            if x < self.diff_x1:
+                self.diff_x1 = x
+            elif x > self.diff_x2:
+                self.diff_x2 = x
+            if y < self.diff_y1:
+                self.diff_y1 = y
+            elif y > self.diff_y2:
+                self.diff_y2 = y
 
     def put_pixel(self, x, y, color):
-        self.pixels[x][y] = self.color_map[color]
+        color_val = self.color_map[color]
+        if self.cur_frame[x][y] != color_val:
+            self.cur_frame[x][y] = self.color_map[color]
+            self.add_diff(x, y)
 
     def put_rect(self, x1, y1, x2, y2, color):
+        color_val = self.color_map[color]
         for x in range(x1, x2 + 1):
             for y in range(y1, y2 + 1):
-                self.pixels[x][y] = self.color_map[color]
+                if self.cur_frame[x][y] != color_val:
+                    self.cur_frame[x][y] = color_val
+                    self.add_diff(x, y)
 
-    def next_frame(self, delay=10):
-        if self.frames and self.frames[-1][0] == self.pixels:
-            # If there's no difference between the last frame and
-            # this one, just add a longer delay to the current frame
-            self.frames[-1][1] += delay
-        else:
-            self.frames.append([self.pixels, delay])
-            self.pixels = [[self.pixels[x][y]
-                            for y in range(self.height)]
-                            for x in range(self.width)]
+    def next_frame(self, delay = 10):
+        # No difference between the two frames
+        if self.diff_x1 is None:
+            return
 
-    def write_header(self, file):
-        # Some values used in the gif header
-        color_res = math.ceil(math.log2(len(self.colors))) - 1
-        logical_screen = 1 << 7 | color_res << 4 | color_res
-
-        # Write the GIF version string
-        file.write(b'GIF89a')
-        # Write the canvas width and height
-        file.write(b'%c%c' % (self.width % 256, self.width // 256))
-        file.write(b'%c%c' % (self.height % 256, self.height // 256))
-        # Write tbe packed field
-        file.write(b'%c' % logical_screen)
-        # Write some outdated useless garbage required by the GIF standard
-        file.write(b'\x00\x00')
-        # Write the color table
-        for i in range(1 << color_res + 1):
-            if i < len(self.colors):
-                file.write(bytes(self.colors[i]))
-            else:
-                # If we ran out of colors, but the color table still needs
-                # to be filled out, we just write black repeatedly
-                file.write(b'\x00\x00\x00')
-        # Write the application extension to allow animation
-        file.write(b'\x21\xff\x0bNETSCAPE2.0\x03\x01\x00\x00\x00')
-
-    def write_frame(self, file, new_frame, prev_frame=None):
-        if prev_frame is None:
-            diff_x1, diff_x2 = 0, self.width - 1
-            diff_y1, diff_y2 = 0, self.height - 1
-        else:
-            # Get the corners of where the frames differ
-            diff_x1, diff_x2, diff_y1, diff_y2 = (None,) * 4
-            for x in range(self.width):
-                for y in range(self.height):
-                    if new_frame[0][x][y] != prev_frame[0][x][y]:
-                        if diff_x1 is None:
-                            diff_x1 = x
-                        diff_x2 = x
-                        if diff_y1 is None or diff_y1 > y:
-                            diff_y1 = y
-                        if diff_y2 is None or diff_y2 < y:
-                            diff_y2 = y
-        # Calculate the width of the differing rectangle
-        diff_width = diff_x2 - diff_x1 + 1
-        diff_height = diff_y2 - diff_y1 + 1
+        # Calculate the width of the region to update
+        diff_width = self.diff_x2 - self.diff_x1 + 1
+        diff_height = self.diff_y2 - self.diff_y1 + 1
 
         # Write the first part of the graphic control extension
-        file.write(b'\x21\xf9\x04\x00')
+        self.content += b'\x21\xf9\x04\x00'
         # Write the delay time
-        file.write(b'%c%c' % (new_frame[1] % 256, new_frame[1] // 256))
+        self.content += b'%c%c' % (delay % 256, delay // 256)
         # Write the rest of the graphic control extension
-        file.write(b'\x00\x00')
+        self.content += b'\x00\x00'
         # Write the image separator
-        file.write(b'\x2c')
+        self.content += b'\x2c'
         # Write the image left and top coordinates
-        file.write(b'%c%c' % (diff_x1 % 256, diff_x1 // 256))
-        file.write(b'%c%c' % (diff_y1 % 256, diff_y1 // 256))
+        self.content += b'%c%c' % (self.diff_x1 % 256, self.diff_x1 // 256)
+        self.content += b'%c%c' % (self.diff_y1 % 256, self.diff_y1 // 256)
         # Write the image width and height coordinates
-        file.write(b'%c%c' % (diff_width % 256, diff_width // 256))
-        file.write(b'%c%c' % (diff_height % 256, diff_height // 256))
+        self.content += b'%c%c' % (diff_width % 256, diff_width // 256)
+        self.content += b'%c%c' % (diff_height % 256, diff_height // 256)
         # Write byte to specify we don't want a local color table
-        file.write(b'\x00')
+        self.content += b'\x00'
         # Finally, write the info for the frame
-        self.write_frame_region(file, new_frame[0], diff_x1, diff_y1, diff_x2, diff_y2)
+        self.write_frame_difference()
 
-    def write_frame_region(self, file, frame, x1, y1, x2, y2):
+    def write_frame_difference(self):
         min_code_size = math.ceil(math.log2(len(self.colors)))
         code_length = min_code_size + 1
         clear_code = 2 ** min_code_size
         end_code = clear_code + 1
 
-        file.write(bytes([min_code_size]))
+        self.content += bytes([min_code_size])
         bits = BitString()
 
         index_buffer = tuple()
         code_table = {(i,): i for i in range(clear_code)}
         next_code = end_code + 1
-        for x in range(x1, x2 + 1):
-            for y in range(y1, y2 + 1):
-                val = frame[x][y]
+        for x in range(self.diff_x1, self.diff_x2 + 1):
+            for y in range(self.diff_y1, self.diff_y2 + 1):
+                val = self.cur_frame[x][y]
                 index_buffer += (val,)
                 if index_buffer not in code_table:
                     code_table[index_buffer] = next_code
@@ -142,18 +117,37 @@ class Gif:
         bits.add_bits(code_table[index_buffer], code_length)
         bits.add_bits(end_code, code_length)
 
-        file.write(bits.get_bytes())
-        file.write(b'\x00')
+        self.content += bits.get_bytes()
+        self.content += b'\x00'
+        self.diff_x1 = self.diff_y1 = self.diff_x2 = self.diff_y2 = None
 
-    def write_frames(self, file):
-        prev_frame = None
-        for frame in self.frames:
-            self.write_frame(file, frame, prev_frame)
-            prev_frame = frame
+    def write_header(self):
+        # Some values used in the gif header
+        color_res = math.ceil(math.log2(len(self.colors))) - 1
+        logical_screen = 1 << 7 | color_res << 4 | color_res
 
-    def write(self, filename):
+        # Write the GIF version string
+        self.content += b'GIF89a'
+        # Write the canvas width and height
+        self.content += b'%c%c' % (self.width % 256, self.width // 256)
+        self.content += b'%c%c' % (self.height % 256, self.height // 256)
+        # Write tbe packed field
+        self.content += b'%c' % logical_screen
+        # Write some outdated useless garbage required by the GIF standard
+        self.content += b'\x00\x00'
+        # Write the color table
+        for i in range(1 << color_res + 1):
+            if i < len(self.colors):
+                self.content += bytes(self.colors[i])
+            else:
+                # If we ran out of colors, but the color table still needs
+                # to be filled out, we just write black repeatedly
+                self.content += b'\x00\x00\x00'
+        # Write the application extension to allow animation
+        self.content += b'\x21\xff\x0bNETSCAPE2.0\x03\x01\x00\x00\x00'
+
+    def write_to_file(self, filename):
         with open(filename, 'wb') as file:
-            self.write_header(file)
-            self.write_frames(file)
-            # Trailing byte, indicates the end of the file
+            file.write(self.content)
+            # Write ending byte
             file.write(b'\x3b')
--- mazes.py
+++ mazes.py
@@ -20,8 +20,8 @@ def recursive_backtracker_gif(width, height, scale):
             return None
 
     def draw_point(x, y, color):
-        maze_gif.put_rect(x * scale, y * scale, (x + 1) * scale,
-                          (y + 1) * scale, color)
+        maze_gif.put_rect(x * scale, y * scale, (x + 1) * scale - 1,
+                          (y + 1) * scale - 1, color)
 
     maze_gif = Gif(width * scale, height * scale, colors)
     cur_maze = [[True for y in range(height)] for x in range(width)]
@@ -41,6 +41,9 @@ def recursive_backtracker_gif(width, height, scale):
             if points:
                 draw_point((x + points[-1][0]) // 2,
                            (y + points[-1][1]) // 2, HALL_COLOR)
+                maze_gif.next_frame(3)
+            else:
+                maze_gif.next_frame(1000)
         else:
             new_x, new_y = new_point
             cur_maze[new_x][new_y] = False
@@ -48,9 +51,7 @@ def recursive_backtracker_gif(width, height, scale):
             cur_maze[(x + new_x) // 2][(y + new_y) // 2] = False
             draw_point((x + new_x) // 2, (y + new_y) // 2, PATH_COLOR)
             points.append(new_point)
-        maze_gif.next_frame(1)
-
-    maze_gif.next_frame(250)
+            maze_gif.next_frame(5)
 
     return maze_gif
 
@@ -61,5 +62,5 @@ HALL_COLOR = (255, 255, 255)
 colors = [WALL_COLOR, PATH_COLOR, HALL_COLOR]
 
 if __name__ == '__main__':
-    gif = recursive_backtracker_gif(21, 21, 5)
-    gif.write('maze.gif')
+    gif = recursive_backtracker_gif(251, 251, 3)
+    gif.write_to_file('maze.gif')
